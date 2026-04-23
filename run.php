215d37a5552d8ff5c1833b6e4794b635e599c55d
ayanshuDS <73418468+MET4L-DS@users.noreply.github.com>
2026-04-23 16:31:39 +0530

    Deployment commit

--- run.php
+++
@@ -1,7 +0,0 @@
-<?php
-require "api/models/User.php";
-require "api/utils/JWTService.php";
-$j = new JWTService();
-$user = new User(3101, "faculty_user", null, null, "faculty");
-echo $j->generateToken($user);
-
--- test_db.php
+++
@@ -1,6 +0,0 @@
-<?php
-require "api/config/DatabaseConfig.php";
-$db = (new DatabaseConfig())->getConnection();
-$stmt = $db->query("DESCRIBE enrollments");
-print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
-
