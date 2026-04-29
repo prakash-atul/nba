1a58e9c601524f3a679e2d5fe69fef8e72f2e62e
ayanshuDS <73418468+MET4L-DS@users.noreply.github.com>
2026-04-30 03:06:23 +0530

    Cleanup

--- frontend/fix.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require("fs");
-let code = fs.readFileSync("src/services/api/faculty.ts", "utf8");
-code = code.replace(/};\r?\n}/, "}");
-fs.writeFileSync("src/services/api/faculty.ts", code);
